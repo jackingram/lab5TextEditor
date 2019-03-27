f7f4c42314dea94798c91febbe8da7053460cd37
jackingram <47607352+jackingram@users.noreply.github.com>
2019-03-27 14:36:14 -0400

    updatedave

--- lab5/lab5/Form1.vb
+++ lab5/lab5/Form1.vb
@@ -27,6 +27,12 @@ Public Class frmMain
     End Sub
 
     Private Sub SaveCtrlSToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveCtrlSToolStripMenuItem.Click
+        '  SaveFileDialog1.Filter = "Text Files|*.txt"
+        '  Dim strFullPath As String = SaveFileDialog1.FileName
+        ' Dim fileWrite As New FileStream(strFullPath, FileMode.Create, FileAccess.Write)
+        ' My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, "" & txtMain.Text, True)
+
+
         Save()
     End Sub
 
@@ -38,10 +44,17 @@ Public Class frmMain
             Dim fileWrite As New FileStream(strFullPath, FileMode.Create, FileAccess.Write)
             Dim writer As New StreamWriter(fileWrite)
             writer.Close()
+            System.IO.File.WriteAllText(SaveFileDialog1.FileName, txtMain.Text)
 
         Catch ex As Exception
             MessageBox.Show("Error")
         End Try
     End Sub
 
+    Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
+        SaveFileDialog1.Filter = "Text Files|*.txt"
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            System.IO.File.WriteAllText(SaveFileDialog1.FileName, txtMain.Text)
+        End If
+    End Sub
 End Class
